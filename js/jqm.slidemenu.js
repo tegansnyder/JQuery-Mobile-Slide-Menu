5dd76529ff1a656ad40e7c2d67b4e9b3ef8a0c68
Tegan Snyder <tsnyder@tegdesign.com>
2012-05-16 15:55:34 -0500

    first commit

---
+++ js/jqm.slidemenu.js
@@ -0,0 +1,71 @@
+$(document).on("pageinit",":jqmData(role='page')", function(){
+
+	var sm = $($(":jqmData(slidemenu)").data('slidemenu'));
+	sm.addClass('slidemenu');
+
+	$(document).on("swipeleft",":jqmData(role='page')", function(){
+		slidemenu(sm);
+	});
+	$(document).on("swiperight",":jqmData(role='page')", function(){
+		slidemenu(sm);
+	});
+	
+	$(document).on("click", ":jqmData(slidemenu)", function() {
+		slidemenu(sm);
+	});
+	
+	$(window).on('resize', function(){
+	
+		if ($(":jqmData(slidemenu)").data('slideopen')) {
+		
+			var sm = $($(":jqmData(slidemenu)").data('slidemenu'));
+			var w = '240px';
+			
+			sm.css('width', w);
+			sm.height(viewport().height);
+		
+			$(":jqmData(role='page')").css('left', w);
+		}
+		
+	});
+
+});
+
+function slidemenu(sm) {
+
+	sm.height(viewport().height);
+	
+	if (!$(this).data('slideopen')) {
+	
+		sm.show();
+		var w = '240px';
+		sm.animate({'width': w}, 'fast');
+		$(":jqmData(role='page')").css('left', w);
+		$(this).data('slideopen', true);
+
+		if ($(":jqmData(role='header')").data('position') == 'fixed') {
+			$(":jqmData(slidemenu)").css('margin-left', parseInt(w.split('px')[0]) + 10 + 'px');
+		} else {
+			$(":jqmData(slidemenu)").css('margin-left', '10px');
+		}
+		
+	} else {
+	
+		var w = '0px';
+		sm.animate({'width': w}, 'fast', function(){sm.hide()});
+		$(":jqmData(role='page')").css('left', w);
+		$(this).data('slideopen', false);
+		
+		$(":jqmData(slidemenu)").css('margin-left', '0px');
+	}
+}
+
+function viewport(){
+	var e = window;
+	var a = 'inner';
+	if (!('innerWidth' in window)) {
+		a = 'client';
+		e = document.documentElement || document.body;
+	}
+	return { width : e[ a+'Width' ] , height : e[ a+'Height' ] }
+}
