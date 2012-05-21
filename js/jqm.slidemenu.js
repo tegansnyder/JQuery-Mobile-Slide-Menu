0cf1d8873a1141a8baaa140c1625ba2b0e781d54
Tegan Snyder <tsnyder@tegdesign.com>
2012-05-21 15:35:25 -0500

    multipage ajax support

--- js/jqm.slidemenu.js
+++ js/jqm.slidemenu.js
@@ -1,22 +1,38 @@
+$(document).on("pagebeforechange", function(event) {
+
+
+
+});
+
 $(document).on("pageinit",":jqmData(role='page')", function(){
 
 	$(":jqmData(slidemenu)").addClass('slidemenu_btn');
 	var sm = $($(":jqmData(slidemenu)").data('slidemenu'));
 	sm.addClass('slidemenu');
+	
 
-	$(document).on("swipeleft",":jqmData(role='page')", function(){
-		slidemenu(sm);
-	});
-	$(document).on("swiperight",":jqmData(role='page')", function(){
-		slidemenu(sm);
+	$(document).on("swiperight swipeleft",":jqmData(role='page')", function(event){
+		event.stopImmediatePropagation();
+		slidemenu(sm, false, $(".ui-page-active").attr('id'));
 	});
 	$(document).on("click", ":jqmData(slidemenu)", function(event) {
 		event.stopImmediatePropagation();
-		slidemenu(sm);
+		slidemenu(sm, false, $(".ui-page-active").attr('id'));
 	});
-	$(document).on("click", "a:not(:jqmData(slidemenu))", function(e) {
-		only_close = true;
-		slidemenu(sm, only_close);
+	
+	
+	
+	$(document).on("click", "a:not(:jqmData(slidemenu))", function(event) {
+		event.stopImmediatePropagation();
+		var page_id;
+		if ($(this).attr('href')) {
+			page_id = $(this).attr('href').split('#')[1];
+		}
+		
+		slidemenu(sm, true, page_id);
+		
+		console.log('test');
+		
 	});
 
 	$(window).on('resize', function(){
@@ -36,7 +52,9 @@ $(document).on("pageinit",":jqmData(role='page')", function(){
 
 });
 
-function slidemenu(sm, only_close) {
+function slidemenu(sm, only_close, page_id) {
+
+	
 
 	sm.height(viewport().height);
 
@@ -45,19 +63,39 @@ function slidemenu(sm, only_close) {
 		sm.show();
 		var w = '240px';
 		sm.animate({width: w, avoidTransforms: false, useTranslate3d: true}, 'fast');
-		$(":jqmData(role='page')").css('left', w);
+		
+		if(page_id) {
+			$(":jqmData(role='page'):not('#" + page_id + "')").removeClass('ui-page-active');
+			$("#" + page_id).addClass('ui-page-active').css('left', w).page();
+		} else {
+			$(":jqmData(role='page')").css('left', w);
+		}
+		
+		
 		$(this).data('slideopen', true);
 
 		if ($(":jqmData(role='header')").data('position') == 'fixed') {
+
 			$(":jqmData(slidemenu)").css('margin-left', parseInt(w.split('px')[0]) + 10 + 'px');
+
 		} else {
 			$(":jqmData(slidemenu)").css('margin-left', '10px');
 		}
 
 	} else {
+	
+		console.log(sm, only_close, page_id);
+	
 		var w = '0px';
 		sm.animate({width: w, avoidTransforms: false, useTranslate3d: true}, 'fast', function(){sm.hide()});
-		$(":jqmData(role='page')").css('left', w);
+		
+		if(page_id) {
+			$(":jqmData(role='page'):not('#" + page_id + "')").removeClass('ui-page-active');
+			$("#" + page_id).addClass('ui-page-active').css('left', w).page();
+		} else {
+			$(":jqmData(role='page')").css('left', w);
+		}
+		
 		$(this).data('slideopen', false);
 		$(":jqmData(slidemenu)").css('margin-left', '0px');
 	}
